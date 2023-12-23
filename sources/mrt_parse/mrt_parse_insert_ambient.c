c6506341c5ae1d8c9a781ec0817d6663f079bf81
Haeseong Kang <gotjd7837@gmail.com>
2023-12-23 22:28:39 +0900

    fix : insert 에러문 수정 필요

--- sources/mrt_parse/mrt_parse_insert_ambient.c
+++ sources/mrt_parse/mrt_parse_insert_ambient.c
@@ -6,7 +6,7 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 18:14:49 by haekang           #+#    #+#             */
-/*   Updated: 2023/12/23 20:56:00 by haekang          ###   ########.fr       */
+/*   Updated: 2023/12/23 22:26:43 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@ void	mrt_parse_insert_ambient(char **line_info, t_info *info, int *flags)
 {
 	*flags = mrt_bit_increase(*flags, AMBIENT_IDX);
 	if (line_info[1] == NULL)
-		mrt_print_err("Incorrect information for Ambient");
+		mrt_print_err("Do not input ratio for Ambient\n");
 	else
 		mrt_parse_insert_ambient_ratio();
 	if (line_info[2] == NULL)
-		mrt_print_err("Incorrect information for Ambient");
+		mrt_print_err("Do not input RGB for Ambient\n");
 	else
 		mrt_parse_insert_ambient_color();
+	if (line_info[3] == NULL)
+		return ;
+	else
+		mrt_print_err("Incorrect information for Ambient\n");
 }
--- sources/mrt_parse/mrt_parse_insert_camera.c
+++ sources/mrt_parse/mrt_parse_insert_camera.c
@@ -6,7 +6,7 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 18:14:52 by haekang           #+#    #+#             */
-/*   Updated: 2023/12/23 20:35:19 by haekang          ###   ########.fr       */
+/*   Updated: 2023/12/23 22:27:39 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	mrt_parse_insert_camera(char **line_info, t_info *info, int *flags)
 {
-    *flags = mrt_bit_increase(*flags, CAMERA_IDX);
+	*flags = mrt_bit_increase(*flags, CAMERA_IDX);
+	if (line_info[1] == NULL)
+		mrt_print_err("Do not input ratio for Ambient\n");
+	else
+		mrt_parse_insert_ambient_ratio();
+	if (line_info[2] == NULL)
+		mrt_print_err("Do not input RGB for Ambient\n");
+	else
+		mrt_parse_insert_ambient_color();
+	if (line_info[3] == NULL)
+		return ;
+	else
+		mrt_print_err("Incorrect information for Ambient\n");
 }
