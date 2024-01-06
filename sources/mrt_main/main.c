6e8c8534e4850caa0831d7e1af30c7e0a8c40929
Haeseong Kang <gotjd7837@gmail.com>
2024-01-06 14:25:39 +0900

    test_render

--- sources/mrt_main/main.c
+++ sources/mrt_main/main.c
@@ -6,45 +6,332 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 16:38:41 by haekang           #+#    #+#             */
-/*   Updated: 2024/01/05 17:30:01 by haekang          ###   ########.fr       */
+/*   Updated: 2024/01/06 14:21:58 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
-void	mrt_print_info(t_info *info)
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+typedef struct s_vec3 t_vec3;
+typedef struct s_vec3 t_point3;
+typedef struct s_vec3 t_color3;
+
+struct s_vec3
 {
-	printf("am: %f\n", info->ambient->ratio);
-	printf("am: %d %d %d\n", info->ambient->color.r, info->ambient->color.g, info->ambient->color.b);
-	printf("cam: %f %f %f\n", info->camera->point.x, info->camera->point.y, info->camera->point.z);
-	printf("cam: %f %f %f\n", info->camera->vector.x, info->camera->vector.y, info->camera->vector.z);
-	printf("cam: %f\n", info->camera->fov);
-	printf("light: %f %f %f\n", info->light->point.x, info->light->point.y, info->light->point.z);
-	printf("light: %f\n", info->light->ratio);
-	printf("light: %d %d %d\n", info->light->color.r, info->light->color.g, info->light->color.b);
-	printf("sphere: %f %f %f\n", ((t_sphere*)info->sphere->content)->point.x, ((t_sphere*)info->sphere->content)->point.y, ((t_sphere*)info->sphere->content)->point.z);
-	printf("sphere: %f\n", ((t_sphere*)info->sphere->content)->diameter);
-	printf("sphere: %d %d %d\n", ((t_sphere*)info->sphere->content)->color.r, ((t_sphere*)info->sphere->content)->color.g, ((t_sphere*)info->sphere->content)->color.b);
-	printf("plane: %f %f %f\n", ((t_plane*)info->plane->content)->point.x, ((t_plane*)info->plane->content)->point.y, ((t_plane*)info->plane->content)->point.z);
-	printf("plane: %f %f %f\n", ((t_plane*)info->plane->content)->vector.x, ((t_plane*)info->plane->content)->vector.y, ((t_plane*)info->plane->content)->vector.z);
-	printf("plane: %d %d %d\n", ((t_plane*)info->plane->content)->color.r, ((t_plane*)info->plane->content)->color.g, ((t_plane*)info->plane->content)->color.b);
-	printf("cylinder: %f %f %f\n", ((t_cylinder*)info->cylinder->content)->point.x, ((t_cylinder*)info->cylinder->content)->point.y, ((t_cylinder*)info->cylinder->content)->point.z);
-	printf("cylinder: %f %f %f\n", ((t_cylinder*)info->cylinder->content)->vector.x, ((t_cylinder*)info->cylinder->content)->vector.y, ((t_cylinder*)info->cylinder->content)->vector.z);
-	printf("cylinder: %f %f\n", ((t_cylinder*)info->cylinder->content)->diameter, ((t_cylinder*)info->cylinder->content)->height);
-	printf("cylinder: %d %d %d\n", ((t_cylinder*)info->cylinder->content)->color.r, ((t_cylinder*)info->cylinder->content)->color.g, ((t_cylinder*)info->cylinder->content)->color.b);
+    double x;
+    double y;
+    double z;
+};
 
-	//범위 제한 아직 구현안함
+typedef struct  s_ray
+{
+    t_point3    orig;
+    t_vec3      dir;
+}   t_ray;
 
+
+typedef struct  s_canvas
+{
+    int     width; //canvas width
+    int     height; //canvas height;
+    double  aspect_ratio; //종횡비
+}           t_canvas;
+
+typedef struct  s_camera_2
+{
+    t_point3    orig;  // 카메라 원점(위치)
+    double      viewport_h; // 뷰포트 세로길이
+    double      viewport_w; // 뷰포트 가로길이
+    t_vec3      horizontal; // 수평길이 벡터
+    t_vec3      vertical; // 수직길이 벡터
+    double      focal_len; // focal length
+    t_point3    left_bottom; // 왼쪽 아래 코너점
+}   t_camera_2;
+
+//벡터3 생성자
+t_vec3      vec3(double x, double y, double z)
+{
+    t_vec3 vec;
+
+    vec.x = x;
+    vec.y = y;
+    vec.z = z;
+    return (vec);
 }
 
-int	main(int ac, char *av[])
+//포인트3 생성자
+t_point3    point3(double x, double y, double z)
 {
-	t_info	*info;
+    t_point3 point;
 
-	if (ac != 2)
-		mrt_print_err("Invalid arguments\n");
-	info = mrt_parse(av[1]);
+    point.x = x;
+    point.y = y;
+    point.z = z;
+    return (point);
+}
 
-	mrt_print_info(info);
-	return (0);
+//색상3 생성자
+t_color3    color3(double r, double g, double b)
+{
+    t_color3 color;
+
+    color.x = r;
+    color.y = g;
+    color.z = b;
+    return (color);
+}
+
+// 벡터 값 설정
+void        vset(t_vec3 *vec, double x, double y, double z)
+{
+    vec->x = x;
+    vec->y = y;
+    vec->z = z;
+}
+
+// 벡터 길이 제곱
+double      vlength2(t_vec3 vec)
+{
+    return (vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
+
+// 벡터 길이
+double      vlength(t_vec3 vec)
+{
+    return (sqrt(vlength2(vec)));
+}
+
+// 벡터합
+t_vec3      vplus(t_vec3 vec, t_vec3 vec2)
+{
+    vec.x += vec2.x;
+    vec.y += vec2.y;
+    vec.z += vec2.z;
+    return (vec);
+}
+
+// 벡터합2
+t_vec3      vplus_(t_vec3 vec, double x, double y, double z)
+{
+    vec.x += x;
+    vec.y += y;
+    vec.z += z;
+    return (vec);
+}
+
+// 벡터차
+t_vec3      vminus(t_vec3 vec, t_vec3 vec2)
+{
+    vec.x -= vec2.x;
+    vec.y -= vec2.y;
+    vec.z -= vec2.z;
+    return (vec);
+}
+
+t_vec3      vminus_(t_vec3 vec, double x, double y, double z)
+{
+    vec.x -= x;
+    vec.y -= y;
+    vec.z -= z;
+    return (vec);
+}
+
+// 벡터 * 스칼라 곱연산
+t_vec3      vmult(t_vec3 vec, double t)
+{
+    vec.x *= t;
+    vec.y *= t;
+    vec.z *= t;
+    return (vec);
+}
+
+// 벡터 축 값끼리 곱연산
+t_vec3      vmult_(t_vec3 vec, t_vec3 vec2)
+{
+    vec.x *= vec2.x;
+    vec.y *= vec2.y;
+    vec.z *= vec2.z;
+    return (vec);
+}
+
+// 벡터 스칼라 나누기
+t_vec3      vdivide(t_vec3 vec, double t)
+{
+    vec.x *= 1 / t;
+    vec.y *= 1 / t;
+    vec.z *= 1 / t;
+
+    return vec;
+}
+
+// 벡터 내적
+double      vdot(t_vec3 vec, t_vec3 vec2)
+{
+    return (vec.x * vec2.x + vec.y * vec2.y + vec.z * vec2.z);
+}
+
+// 벡터 외적
+t_vec3      vcross(t_vec3 vec, t_vec3 vec2)
+{
+    t_vec3 new;
+
+    new.x = vec.y * vec2.z - vec.z * vec2.y;
+    new.y = vec.z * vec2.x - vec.x * vec2.z;
+    new.z = vec.x * vec2.y - vec.y * vec2.x;
+    return (new);
+}
+
+// 단위 벡터
+t_vec3      vunit(t_vec3 vec)
+{
+    double len = vlength(vec);
+    if (len == 0)
+    {
+        printf("Error\n:Devider is 0");
+        exit(0);
+    }
+    vec.x /= len;
+    vec.y /= len;
+    vec.z /= len;
+    return (vec);
+}
+
+// 두 벡터의 원소를 비교하여 작은 값들만 반환
+t_vec3  vmin(t_vec3 vec1, t_vec3 vec2)
+{
+    if (vec1.x > vec2.x)
+        vec1.x = vec2.x;
+    if (vec1.y > vec2.y)
+        vec1.y = vec2.y;
+    if (vec1.z > vec2.z)
+        vec1.z = vec2.z;
+    return (vec1);
+}
+
+// [0,1] 로 되어있는 rgb 값을 각각 [0,255]에 맵핑 해서 출력.
+void    write_color(t_color3 pixel_color)
+{
+    printf("%d %d %d\n", (int)(255.999 * pixel_color.x),
+                        (int)(255.999 * pixel_color.y),
+                        (int)(255.999 * pixel_color.z));
+}
+
+//ray 생성자(정규화 된 ray)
+t_ray       ray(t_point3 orig, t_vec3 dir)
+{
+    t_ray ray;
+
+    ray.orig = orig;
+    ray.dir = vunit(dir);
+    return (ray);
+}
+
+//ray origin point 부터 방향벡터 ray dir * t 만큼 떨어진 점.
+t_point3    ray_at(t_ray *ray, double t)
+{
+    t_point3 at;
+
+    at = vplus(ray->orig, vmult(ray->dir, t));
+    return (at);
+}
+
+t_canvas    canvas(int  width, int height)
+{
+    t_canvas canvas;
+
+    canvas.width = width;
+    canvas.height = height;
+    canvas.aspect_ratio = (double)width / (double)height;
+    return (canvas);
+}
+
+t_camera_2    camera_2(t_canvas *canvas, t_point3 orig)
+{
+    t_camera_2    cam;
+    double      focal_len;
+    double      viewport_height;
+
+    viewport_height = 2.0;
+    focal_len = 1.0;
+    cam.orig = orig;
+    cam.viewport_h = viewport_height;
+    cam.viewport_w = viewport_height * canvas->aspect_ratio;
+    cam.focal_len = focal_len;
+    cam.horizontal = vec3(cam.viewport_w, 0, 0);
+    cam.vertical = vec3(0, cam.viewport_h, 0);
+    // 왼쪽 아래 코너점 좌표, origin - horizontal / 2 - vertical / 2 - vec3(0,0,focal_length)
+    cam.left_bottom = vminus(vminus(vminus(cam.orig, vdivide(cam.horizontal, 2)),
+                                vdivide(cam.vertical, 2)), vec3(0, 0, focal_len));
+    return (cam);
+}
+
+//primary_ray 생성자
+t_ray       ray_primary(t_camera_2 *cam, double u, double v)
+{
+    t_ray   ray;
+
+    ray.orig = cam->orig;
+    // left_bottom + u * horizontal + v * vertical - origin 의 단위 벡터.
+    ray.dir = vunit(vminus(vplus(vplus(cam->left_bottom, vmult(cam->horizontal, u)), vmult(cam->vertical, v)), cam->orig));
+    return (ray);
+}
+
+//광선이 최종적으로 얻게된 픽셀의 색상 값을 리턴.
+t_color3    ray_color(t_ray *r)
+{
+    double  t;
+
+    t = 0.5 * (r->dir.y + 1.0);
+    // (1-t) * 흰색 + t * 하늘색
+    return (vplus(vmult(color3(1, 1, 1), 1.0 - t), vmult(color3(0.5, 0.7, 1.0), t)));
+}
+
+int main(void)
+{
+    int         i;
+    int         j;
+    /* * * * 추가 * * * */
+    double      u;
+    double      v;
+    /* * * * 추가 끝 * * * */
+
+    t_color3    pixel_color;
+    /* * * * 수정 * * * */
+    t_canvas    canv;
+    t_camera_2    cam;
+    t_ray       ray;
+
+    //Scene setting;
+    canv = canvas(400, 300);
+    cam = camera_2(&canv, point3(0, 0, 0));
+    /* * * * 수정 끝 * * * */
+
+    // 랜더링
+    // P3 는 색상값이 아스키코드라는 뜻, 그리고 다음 줄은 캔버스의 가로, 세로 픽셀 수, 마지막은 사용할 색상값
+    /* * * * 수정 * * * */
+    printf("P3\n%d %d\n255\n", canv.width, canv.height);
+    j = canv.height - 1;
+    /* * * * 수정 끝 * * * */
+    while (j >= 0)
+    {
+        i = 0;
+        /* * * * 수정 * * * */
+        while (i < canv.width)
+        {
+            u = (double)i / (canv.width - 1);
+            v = (double)j / (canv.height - 1);
+            //ray from camera_2 origin to pixel
+            ray = ray_primary(&cam, u, v);
+            pixel_color = ray_color(&ray);
+        /* * * * 수정 끝 * * * */
+            write_color(pixel_color);
+            ++i;
+        }
+        --j;
+    }
 }
