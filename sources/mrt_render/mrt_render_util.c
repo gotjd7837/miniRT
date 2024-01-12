0478e87612a8a298b16a029da691a77fa6edd0e6
Haeseong Kang <gotjd7837@gmail.com>
2024-01-13 02:20:32 +0900

    feat : light

--- sources/mrt_render/mrt_render_util.c
+++ sources/mrt_render/mrt_render_util.c
@@ -6,7 +6,7 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 07:45:08 by haekang           #+#    #+#             */
-/*   Updated: 2024/01/12 20:33:13 by haekang          ###   ########.fr       */
+/*   Updated: 2024/01/13 01:35:46 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	mrt_put_pixel(t_info *info, int x, int y, t_color color)
 	r = color.x;
 	g = color.y;
 	b = color.z;
-	rgb = ((int)r << 16) + ((int)g << 8) + ((int)b);
+	rgb = ((int)(r * 255.999) << 16) + ((int)(g * 255.999) << 8) + (int)(b * 255.999);
 	dst = info->img.addr + (y * info->img.line_length + x
 			* (info->img.bits_per_pixel / 8));
 	*(unsigned int *)dst = rgb;
